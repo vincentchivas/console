da4dfddd10a2ddfeb97fecfe8b631aed5464578e
bhuang <bhuang@bainainfo.com>
2015-08-10 01:28:40 -0700

    [feautre]add bootstrap base, cool

--- com_admin/cm_console_service/cm_console/webfront/web_api/test.py
+++ com_admin/cm_console_service/cm_console/webfront/web_api/test.py
@@ -13,6 +13,16 @@ def index1():
     return render_template('hello1.htm')
 
 
+@app.route('/webfront/extend', methods=['GET', ])
+def index2():
+    return render_template('child.html')
+
+
+@app.route('/webfront/hello2', methods=['GET', ])
+def index3():
+    return render_template('login.html')
+
+
 @app.route('/webfront/themelocale', methods=['GET', ])
 def show_theme():
     return render_template('themelocale/add.htm')
