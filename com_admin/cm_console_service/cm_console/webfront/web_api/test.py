e0ad65f5ea07baa97f3c42aea8c30bdeb6c706a4
bhuang <bhuang@bainainfo.com>
2015-08-10 23:45:23 -0700

    [feature]validate html, but some bug didnt fix, such as left nav

--- com_admin/cm_console_service/cm_console/webfront/web_api/test.py
+++ com_admin/cm_console_service/cm_console/webfront/web_api/test.py
@@ -3,24 +3,10 @@ from flask import render_template
 from cm_console.api import app
 
 
-@app.route('/webfront/hello', methods=['GET', ])
-def index():
-    return render_template('hello.html')
-
-
-@app.route('/webfront/hello1', methods=['GET', ])
-def index1():
-    return render_template('hello1.htm')
-
-
-@app.route('/webfront/extend', methods=['GET', ])
-def index2():
-    return render_template('child.html')
-
-
-@app.route('/webfront/hello2', methods=['GET', ])
-def index3():
-    return render_template('login.html')
+@app.route('/webfront/<tmp_name>', methods=['GET', ])
+def render_tml(tmp_name):
+    tml_src = 'resources/%s' % tmp_name
+    return render_template(tml_src)
 
 
 @app.route('/webfront/themelocale', methods=['GET', ])
